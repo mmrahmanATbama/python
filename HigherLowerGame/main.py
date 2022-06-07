cfa58ad47a98a000d71bc1774737b9601f2ab6ca
Mohammed Rahman <mmrahman.bama@gmail.com>
2022-06-06 19:34:49 -0500

    Higher lower game working

--- HigherLowerGame/main.py
+++ HigherLowerGame/main.py
@@ -1,51 +1,60 @@
-# show Higher Lower art
-# Compare A: Biilie Ellish, a Musician, from United States.
-
-# Show Vs. logo
-# Against B: Shawn Mendes, a Musician, from Canada.
-# Who has more followers? Type 'A' or 'B':
-# Get Answer:
-# Logo
-# if Correct You're right! Current score: 1  Repeat
-# else: Sorry, that's wrong. Final score:
-
-
 import random
-
 from art import logo
 from art import vs
 from game_data import data
 
-# Get a random data from a list of dictionaries. Call it Compare A:
-# Get a random data from a list of dictionaries, Call it Against B:
-# Ensure A and B are not identical.
-# get input from user.
-# Compare and give answer.
 
-def get_user_input():
+current_score = 0
+
+
+def increase_current_score():
+    global current_score
+    current_score += 1
+    return current_score
+
+
+def result(ind_a, ind_b, ans):
+    a_follower_count = data[ind_a]['follower_count']
+    b_follower_count = data[ind_b]['follower_count']
     print(logo)
-    index_A = 0
-    index_B = 0
+    if ans == 'A' and a_follower_count > b_follower_count:
+        increase_current_score()
+        print(f"You're right. Current score: {current_score}")
+        return ind_b
+    elif ans == 'B' and b_follower_count > a_follower_count:
+        increase_current_score()
+        print(f"You're right. Current score: {current_score}")
+        return ind_b
+    else:
+        print(f"Sorry, that's wrong. Final score: {current_score}")
+        return -1
 
-    while index_A == index_B:
-        index_A = random.randint(0, len(data) - 1)
-        index_B = random.randint(0, len(data) - 1)
 
-# {
-#     'name': 'Ariana Grande',
-#     'follower_count': 183,
-#     'description': 'Musician and actress',
-#     'country': 'United States'
-# },
-    A_follower = data[index_A]['follower_count']
-    B_follower = data[index_B]['follower_count']
-    index_A = 0
-    print(f"Compare A: {data[index_A]['name']}, {data[index_A]['description']}, {data[index_A]['country']}")
+def get_user_choice(ind_a, ind_b):
+    print(f"Compare A: {data[ind_a]['name']}, {data[ind_a]['description']}, {data[ind_a]['country']}")
     print(vs)
-    print(f"Against B: {data[index_A]['name']}, {data[index_A]['description']}, {data[index_A]['country']}")
-    answer = input("Who has more followers? Type 'A' or 'B': ").lower()
+    print(f"Against B: {data[ind_b]['name']}, {data[ind_b]['description']}, {data[ind_b]['country']}")
+    answer = input("Who has more followers? Type 'A' or 'B': ").capitalize()
+    return answer
 
 
+def play():
+    print(logo)
+    index_a = 0
+    index_b = 0
 
-#print(vs)
-get_user_input()
+    while index_a == index_b:
+        index_a = random.randint(0, len(data) - 1)
+        index_b = random.randint(0, len(data) - 1)
+
+    answer = get_user_choice(index_a, index_b)
+    game_play = result(index_a, index_b, answer)
+
+    while game_play != -1:
+        index_a = game_play
+        index_b = random.randint(0, len(data) - 1)
+        answer = get_user_choice(index_a, index_b)
+        game_play = result(index_a, index_b, answer)
+
+
+play()
