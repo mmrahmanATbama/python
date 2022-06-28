6247fd050fef70df502aaa6e35805f9508ba6507
Mohammed Rahman <mmrahman.bama@gmail.com>
2022-06-28 03:08:16 -0500

    Mail Merge

--- Mail_Merge/main.py
+++ Mail_Merge/main.py
@@ -7,8 +7,23 @@
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+guests=[]
 
-# read from the file and put the items in a list:
 with open("/Users/mmrha/PycharmProjects/Mail_Merge/Input/Names/invited_names.txt") as f:
     invited_guests = f.readlines()
 
+for names in invited_guests:
+    name = names.strip("\n")
+    guests.append(name)
+
+replace_text = "[name]"
+
+with open("/Users/mmrha/PycharmProjects/Mail_Merge/Input/Letters/starting_letter.txt") as start:
+    letter = start.read()
+
+
+
+for names in guests:
+    updated_letter = letter.replace(replace_text, names)
+    with open(f"/Users/mmrha/PycharmProjects/Mail_Merge/Output/ReadyToSend/{names}.txt", mode="w") as new_file:
+        new_file.write(updated_letter)
