4c2d410c9a9defddf8b47c4a149b9b1257f13276
Mohammed Rahman <mmrahman.bama@gmail.com>
2022-07-03 23:58:34 -0500

    Tkinter reference

--- Test/main.py
+++ Test/main.py
@@ -1,15 +1,62 @@
+# this is just some random stuff.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("Hello")
-    print("Hello"[0])
-    print("Hello"[len("Hello") - 1])
-    print(len(str(123)))
+import tkinter
+
+window = tkinter.Tk()
+window.title("Test Window")
+window.minsize(width=500, height=300)
+
+# Label
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 18))
+my_label.pack()
 
 
+# Button
+def button_clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+button = tkinter.Button(text="Click me", command=button_clicked)
+button.pack()
 
+# Entry
+input = tkinter.Entry(width=10)
+input.pack()
+
+
+# Advanced Python Arguments
+# veriadic function: unlimited number of arguments
+
+def add(*args):
+    num = 0
+    for number in args:
+        num = num + number
+    return num
+
+
+print(add(1, 2, 3, 4, 5))
+
+
+# **kwargs: May keyworded arguments
+
+def calculate(n, **kwargs):
+    for key, value in kwargs.items():
+        print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+    def __init__(self, **kwargs):
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+
+
+my_car = Car(make="Hyuandai")
+print(my_car.make)
+
+window.mainloop()
